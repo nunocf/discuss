28fdd663a419fa476aff418a07da579cc4bd2ab4
Nuno Ferreira <nuno.ferreira@gamesinc.co.uk>
2018-08-26 16:51:25 +0100

    added OAuth with gihub, yaygit status

--- config/config.exs
+++ config/config.exs
@@ -14,8 +14,7 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "JEqUqkyi4XBR0kOsW8noX43Ld9cRbV8uCoC3lBmmqVJpSlSqNjguaBAHE6/YM3cS",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +23,13 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth, providers: [github: {Ueberauth.Strategy.Github, []}]
+
+config(
+  :ueberauth,
+  Ueberauth.Strategy.Github.OAuth,
+  client_id: "24d213d26daee658271a",
+  client_secret: "f7614a330a415f70e0d6965fef54108a87141e0a"
+)
